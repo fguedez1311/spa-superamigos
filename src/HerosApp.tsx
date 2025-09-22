63dba92e95ea1eebf381f923003837e17ca300f7
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-22 16:05:45 -0400

    Creación de Rutas y Pantallas

---
+++ src/HerosApp.tsx
@@ -0,0 +1,11 @@
+import { RouterProvider } from "react-router"
+import { appRouter} from "./router/app.router"
+
+
+export const HerosApp = () => {
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  )
+}
