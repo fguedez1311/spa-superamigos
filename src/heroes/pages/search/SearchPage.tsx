7e4508fed725c23a52e9c003f14b93bf0fa10558
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-23 16:04:13 -0400

    Creado Componentes de búsqueda

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControls } from "./ui/SearchControls"
+
 
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <CustomJumbotron 
+          title="Búsqueda de SuperHeroes"
+          description="Descubre, explora y administra super Héroes y Villanos" 
+              
+      />
+      {/* Stats Dashboard */}
+      <HeroStats/>
+      {/*Filter and search */}
+      <SearchControls/>
+    </>
   )
 }
+
+export default SearchPage
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,10 +1,13 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/Layouts/HeroesLayout";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
+
+const SearchPage=lazy(()=>import('@/heroes/pages/search/SearchPage'))
 
 export const appRouter=createBrowserRouter([
     {
