2a30fbfcafe450be794578ad6721666d42130c44
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-10-08 15:19:51 -0400

    Se Creo el componente de paginación

--- src/components/custom/CustomPagination.tsx
+++ src/components/custom/CustomPagination.tsx
@@ -1,18 +1,31 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { Button } from '../ui/button'
+import { useSearchParams } from 'react-router'
 
 interface Props{
   totalPages:number,
   
 }
 export const CustomPagination = ({totalPages}:Props) => {
-  const page:number=1 
+
+  const [searchParams,setSearchParams]=useSearchParams()
+  
+  const queryPage=searchParams.get('page') ?? '1'
+  const page=isNaN(+queryPage) ? 1 :+queryPage
+
+  const handlePageChange=(page:number)=>{
+      if (page<1 || page>totalPages) return
+
+      searchParams.set('page',page.toString())
+      setSearchParams(searchParams)
+  }
   return (
     <div className="flex items-center justify-center space-x-2">
 
           <Button
                variant="outline" size="sm"
                disabled={page===1}
+               onClick={()=>handlePageChange(page-1)}
           >
             <ChevronLeft className="h-4 w-4" />
             Anteriores
@@ -24,6 +37,7 @@ export const CustomPagination = ({totalPages}:Props) => {
                     key={index}
                     variant={page===index+1 ? 'default' :'outline'} 
                     size="sm"
+                    onClick={()=>handlePageChange(index+1)}
               
               >
                 {index+1}
@@ -39,6 +53,7 @@ export const CustomPagination = ({totalPages}:Props) => {
           <Button 
               variant="outline" size="sm"
               disabled={page===totalPages}
+              onClick={()=>handlePageChange(page+1)}
           
           >
             Siguientes
