8ef49bd103f3df433238c6b086871ee0c377d6c7
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-10-14 15:49:32 -0400

    Creación del HeroPage

---
+++ src/heroes/hooks/useSuperHero.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+import React from 'react'
+import { getHero } from '../actions/get-hero'
+
+export const useSuperHero = (idSlug:string) => {
+  return    useQuery({
+                    queryKey:['hero',{idSlug}],
+                    queryFn:()=>getHero(idSlug),
+                    staleTime:1000*60*5 //5 minutos
+                })
+  
+}
